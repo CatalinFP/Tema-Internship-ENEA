faa1649cd794bcc8a69992880653b402c84a28f2
CatalinFP <93130961+CatalinFP@users.noreply.github.com>
2021-10-28 11:44:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,42 +7,38 @@ from record_audio import record_audio
 from record_video import record_video
 from db_extract import extract_db_file
 import subprocess
-import logging
 
 
 if __name__ == '__main__':
 
     try:
-        # search_ytb = Thread(target=search_youtube(search_key = "python"))
-        # rec_vid = Thread(target=record_video)
-        # rec_aud = Thread(target=record_audio)
+        search_ytb = Thread(target=search_youtube(search_key = "python"))
+        rec_vid = Thread(target=record_video)
+        rec_aud = Thread(target=record_audio)
         db = Thread(target=extract_db_file)
 
-        # search_ytb.start()
-        # search_ytb.join()
+        search_ytb.start()
+        search_ytb.join()
 
-        # rec_vid.start()
-        # rec_vid.join()
-        # rec_aud.start()
-        # rec_aud.join()
+        rec_vid.start()
+        rec_vid.join()
+        rec_aud.start()
+        rec_aud.join()
 
     except TimeoutException as timerr:
         print(timerr)
 
-    except WebDriverException as weberr:
-        msg = str(webdriver.msg)
-        
-        if 'ERR_INTERNET_DISCONNECTED' in msg:
-            print("No internet connection!")
+    except Exception as weberr:
+        raise WebDriverException("No internet connection!", weberr)
 
     try:
         subprocess.run(["ffmpeg -i record_video.avi -i record_audio.wav -c:v copy -c:a aac output.mp4"], check=True) #combine video and audio file
     
-    except subprocess.CalledProcessError:
-        logging.exception("Missing Files!")
+    except subprocess.CalledProcessError as logerr:
+        print("Missing Files!", logerr)
     
 
     try:
         db.start()
-    except Exception:
-        print("No such file or directory!")
+    except Exception as e:
+        print("No such file or directory!", e)
