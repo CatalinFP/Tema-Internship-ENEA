36c154075fe2a0bda6972fd180d7f40c748d7dfa
CatalinFP <93130961+CatalinFP@users.noreply.github.com>
2021-10-28 11:43:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 from threading import Thread
+import logger
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, WebDriverException
 from search_youtube import search_youtube
 from record_audio import record_audio
 from record_video import record_video
 from db_extract import extract_db_file
-import os
+import subprocess
+import logging
 
 
 if __name__ == '__main__':
-    search_ytb = Thread(target=search_youtube(search_key = "python"))
-    search_ytb.start()
-    search_ytb.join()
 
-    rec_vid = Thread(target=record_video)
-    rec_aud = Thread(target=record_audio)
+    try:
+        # search_ytb = Thread(target=search_youtube(search_key = "python"))
+        # rec_vid = Thread(target=record_video)
+        # rec_aud = Thread(target=record_audio)
+        db = Thread(target=extract_db_file)
 
-    rec_vid.start()
-    rec_vid.join()
-    rec_aud.start()
-    rec_aud.join()
+        # search_ytb.start()
+        # search_ytb.join()
 
-    os.system("ffmpeg -i record_video.avi -i record_audio.wav -c:v copy -c:a aac output.mp4") #combine video and audio file
+        # rec_vid.start()
+        # rec_vid.join()
+        # rec_aud.start()
+        # rec_aud.join()
 
-    db = Thread(target=extract_db_file)
-    db.start()
+    except TimeoutException as timerr:
+        print(timerr)
+
+    except WebDriverException as weberr:
+        msg = str(webdriver.msg)
+        
+        if 'ERR_INTERNET_DISCONNECTED' in msg:
+            print("No internet connection!")
+
+    try:
+        subprocess.run(["ffmpeg -i record_video.avi -i record_audio.wav -c:v copy -c:a aac output.mp4"], check=True) #combine video and audio file
+    
+    except subprocess.CalledProcessError:
+        logging.exception("Missing Files!")
+    
+
+    try:
+        db.start()
+    except Exception:
+        print("No such file or directory!")
